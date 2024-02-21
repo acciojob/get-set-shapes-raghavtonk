e221c7b6a1ce0f17e37e1ffe5330fdf0e2d4d97d
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-21 15:35:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width=width;
+		this.height=height;
+	}
+	
+	getArea(){
+		return this.width * this.height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(side){
+		super(side,side);
+		this.side=side;
+	}
+	getPerimeter(){
+		return this.side*4;
+	}
+}
+// const rectangle = new Rectangle(5, 10);
+// console.log(rectangle.width);
+// console.log(rectangle.height)
+// console.log(rectangle.getArea());
+// const square = new Square(7);
+// console.log(square.width);
+// console.log(square.height);
+// console.log(square.getArea());
+// console.log(square.getPerimeter());
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
